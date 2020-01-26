6f3fd27a24728b7f8a0a39389f79359e48c503be
Victor Maciel <victorvmaciel@gmail.com>
2020-01-26 16:48:09 -0300

    Order, Instant, ISO 8601 part 1

--- src/main/java/com/viktor/javaweb/entities/User.java
+++ src/main/java/com/viktor/javaweb/entities/User.java
@@ -1,13 +1,18 @@
 package com.viktor.javaweb.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="tb_user")
 public class User implements Serializable{
 	
 
@@ -21,7 +26,12 @@ public class User implements Serializable{
 	public String phone;
 	private String password;
 	
+	@OneToMany(mappedBy="client")
+	private List<Order> orders = new ArrayList<>();
 	
+	
+	
+
 	public User() {
 		
 	}
@@ -81,6 +91,10 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
