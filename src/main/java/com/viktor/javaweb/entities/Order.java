80cb1ac4f4d671b6a167beeb50e52aad5ab2c53e
Victor Maciel <victorvmaciel@gmail.com>
2020-01-27 09:55:55 -0300

    OrderStatus enum

--- src/main/java/com/viktor/javaweb/entities/Order.java
+++ src/main/java/com/viktor/javaweb/entities/Order.java
@@ -12,7 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.viktor.javaweb.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -26,18 +26,24 @@ public class Order implements Serializable {
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	
+	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	
+	//Methods
 
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -59,6 +65,17 @@ public class Order implements Serializable {
 
 	public User getClient() {
 		return client;
+	}
+
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if( orderStatus!= null){
+		this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public void setClient(User client) {
